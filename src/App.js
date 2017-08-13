355a52c58935adff49c25002eddabb8d88758607
Kevin Weaver <kevinweaver2@gmail.com>
2017-08-12 20:33:08 -0700

    Working search with 4 components

--- src/App.js
+++ src/App.js
@@ -28,10 +28,11 @@ class App extends Component {
 
     this.state = {
       list,
+      searchTerm: '',
     };
 
     this.onDismiss = this.onDismiss.bind(this);
-
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id) {
@@ -40,29 +41,80 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
+  onSearchChange(event){
+    this.setState({searchTerm: event.target.value});
+  }
+
   render() {
-    const pageTitle = "Hacker News"
+    const pageTitle = "Hacker News";
+    const { searchTerm, list } = this.state;
+
     return (
       <div className="App">
         <h2>{pageTitle}</h2>
-        { this.state.list.map(item =>
-          <div key={item.objectID}>
-            <span><a href={item.url}>{item.title}</a></span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button">
-                Dismiss
-              </button>
-            </span>
-          </div>
-        )}
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        >
+          Search
+        </Search>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onChange={this.onDismiss}
+        />
       </div>
     );
   }
+}
+
+const Search = ({ value, onChange, children }) => {
+  return (
+    <form>
+    {children} <input
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </form>
+  );
+}
+
+const Table = ({ list, pattern, onDismiss }) => {
+  const isSearched = (searchTerm) => (item) =>
+    !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+  return (
+    <div>
+      { list.filter(isSearched(pattern)).map(item =>
+        <div key={item.objectID}>
+          <span><a href={item.url}>{item.title}</a></span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <Button
+              onClick={() => onDismiss(item.objectID)}
+            >
+              Dismiss
+            </Button>
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+const Button = ({ onClick, className = '', children }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={className}
+      type="button"
+    >
+      {children}
+    </button>
+  );
 }
 
 export default App;
